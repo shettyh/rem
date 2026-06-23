d37741b64be0f8dc7981ff6baff4e3fe0e0a0a5e
shettyh <manjunathshetty@live.com>
2026-06-23 17:17:25 +0530

    feat: auto-sync on launch and window hide (desktop only)

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,13 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
-import { StorageProvider } from '../data/StorageContext'
+import { StorageProvider, defaultStorage } from '../data/StorageContext'
+import { useAutoSync } from './useAutoSync'
 import '../ui/styles.css'
+
+function App() {
+  useAutoSync(defaultStorage)
+  return <RouterProvider router={router} />
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <StorageProvider>
-      <RouterProvider router={router} />
+      <App />
     </StorageProvider>
   </StrictMode>,
 )
